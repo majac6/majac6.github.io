9af93458e59d449a02da8cde180806b4e6292594
majac <majac6@gmail.com>
2017-10-31 14:42:49 +0900

    no message

---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  pathPrefix: '/',
+  siteMetadata: {
+    title: "MAJAC Blog",
+    author: "majac6",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        precision: 8,
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: "posts",
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`
+  ],
+}
