b8699421b0b8e946a1c7e917fa9ac1217d350e6e
majac <majac6@gmail.com>
2017-10-31 14:02:52 +0900

    no message

--- src/pages/profile.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Disqus from '../components/Disqus'
-
-export default () => 
-  <div>
-    MY SIMPLE PRFILE
-  </div>
--- src/posts/2017-10-30-start-github-blog/index.md
+++
@@ -1,7 +0,0 @@
----
-title: gatsbyJS 로 블로그 시작하기
-date: "2017-10-30T13:00:00.284Z"
-path: "/start-github-blog/"
----
-
-글 작성 해야함 ㅎㅎ
