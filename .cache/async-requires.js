1cc67806d14fa1ed16224fec1ee36e58b9005b69
majac <majac6@gmail.com>
2017-11-23 15:56:37 +0900

    no message

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -4,8 +4,8 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/dev-404-page.js"),
   "component---src-templates-blog-post-js": require("gatsby-module-loader?name=component---src-templates-blog-post-js!/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/templates/blog-post.js"),
-  "component---src-pages-profile-js": require("gatsby-module-loader?name=component---src-pages-profile-js!/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/pages/profile.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/pages/index.js")
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/pages/index.js"),
+  "component---src-pages-profile-js": require("gatsby-module-loader?name=component---src-pages-profile-js!/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/pages/profile.js")
 }
 
 exports.json = {
@@ -14,9 +14,9 @@ exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/layout-index.json"),
   "start-github-blog.json": require("gatsby-module-loader?name=path---start-github-blog!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/start-github-blog.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/layout-index.json"),
-  "profile.json": require("gatsby-module-loader?name=path---profile!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/profile.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/index.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/layout-index.json"),
-  "index.json": require("gatsby-module-loader?name=path---index!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/index.json")
+  "profile.json": require("gatsby-module-loader?name=path---profile!/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/profile.json")
 }
 
 exports.layouts = {
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,8 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/dev-404-page.js")),
   "component---src-templates-blog-post-js": preferDefault(require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/templates/blog-post.js")),
-  "component---src-pages-profile-js": preferDefault(require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/pages/profile.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/pages/index.js"))
+  "component---src-pages-index-js": preferDefault(require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/pages/index.js")),
+  "component---src-pages-profile-js": preferDefault(require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/src/pages/profile.js"))
 }
 
 exports.json = {
@@ -15,9 +15,9 @@ exports.json = {
   "layout-index.json": require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/layout-index.json"),
   "start-github-blog.json": require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/start-github-blog.json"),
   "layout-index.json": require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/layout-index.json"),
-  "profile.json": require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/profile.json"),
+  "index.json": require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/index.json"),
   "layout-index.json": require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/layout-index.json"),
-  "index.json": require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/index.json")
+  "profile.json": require("/Users/MAJAC/Documents/Project/majac/majac6.github.io/.cache/json/profile.json")
 }
 
 exports.layouts = {
